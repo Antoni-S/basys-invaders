7367e7c552b5463897d7ef9f677c90ea62a95f97
TheLicje <sieja815@gmail.com>
2025-05-29 13:24:15 +0200

    Implementation of background

---
+++ rtl/vga_if.sv
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2025  AGH University of Science and Technology
+ * MTM UEC2
+ * Author: Tomasz Sieja
+ *
+ * Description:
+ * 
+ */
+
+ interface vga_if;
+    logic [10:0] vcount;
+    logic        vsync;
+    logic        vblnk;
+    logic [10:0] hcount;
+    logic        hsync;
+    logic        hblnk;
+    logic [11:0] rgb;
+
+    modport in (
+        input vcount, vsync, vblnk, hcount, hsync, hblnk, rgb
+    );
+
+    modport out (
+        output vcount, vsync, vblnk, hcount, hsync, hblnk, rgb
+    );
+endinterface
